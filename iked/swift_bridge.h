4b37de90c867e799e6295ff14fca222ac7e744b9
SUENAGA Hiroki <hsuenaga@iij.ad.jp>
2026-07-02 10:47:48 +0900

    remove inline fucntions from header

--- iked/swift_bridge.h
+++ iked/swift_bridge.h
@@ -17,53 +17,6 @@ struct swift_bridge {
 
 extern struct swift_bridge *swift_bridge;
 
-static inline bool
-swift_puts(const char *string)
-{
-	if (swift_bridge == NULL)
-		return false;
-	if (swift_bridge->swift_puts == NULL)
-		return false;
-
-	return swift_bridge->swift_puts(string);
-}
-
-static inline int
-swift_vprintf(const char *fmt, va_list ap)
-{
-	if (swift_bridge == NULL)
-		return false;
-	if (swift_bridge->swift_vprintf == NULL)
-		return false;
-
-	return swift_bridge->swift_vprintf(fmt, ap);
-}
-
-static inline int
-swift_printf(const char *fmt, ...)
-{
-	va_list ap;
-	int ret;
-
-	va_start(ap, fmt);
-	ret = swift_vprintf(fmt, ap);
-	va_end(ap);
-
-	return ret;
-}
-
-static inline void
-swift_error(int num, const char *message)
-{
-	if (swift_bridge == NULL)
-		return;
-	if (swift_bridge->swift_error == NULL)
-		return;
-
-	(void) swift_bridge->swift_error(num, message);
-	return;
-}
-
 
 /* XXX: TO BE REMOVED */
 int swift_main(int argc, char *argv[]);
